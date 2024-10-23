c1e636ac029066112e2435b6093b5b4b30dfcd4d
leticyafr <leticya.siqueira@aluno.senai.br>
2024-10-23 11:29:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function criarBotaoFechar(li) {
+    let rotulo = document.createElement("SPAN");
+    let fechar = document.createTextNode("\u00D7");
+  
+    rotulo.className = "close";
+    rotulo.appendChild(fechar);
+    li.appendChild(rotulo);
+  
+    rotulo.onclick = () => {
+      rotulo.parentElement.style.display = "none";
+    };
+  }
+  
+  function adicionarTarefa() {
+    let li = document.createElement("li");
+    let input = document.formPrincipal.tarefas.value;
+    let input_texto = document.createTextNode(input);
+  
+    li.appendChild(input_texto);
+  
+    document.querySelector("ul").appendChild(li);
+    document.formPrincipal.tarefas.value = "";
+  
+    criarBotaoFechar(li);
+  }
+  
+  document.querySelectorAll('li').forEach(criarBotaoFechar)
+  document.querySelector('ul').addEventListener('click', (e) =>{
+      if(e.target.tagName === "LI")
+      e.target.classList.toggle('checked')
+  })
